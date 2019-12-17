1d8c6a5e98f518dfc28ea1a5624d0c7b9920cafd
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-12-17 14:10:35 +0100

    Delete file

--- templates/nginx_user_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo yum update -y
-sudo amazon-linux-extras install nginx1.122 -y
-export HOSTNAME=$(hostname | tr -d '\n')
-export PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-echo "Welcome to $HOSTNAME - $PRIVATE_IP" > /usr/share/nginx/html/index.html
-service nginx start
